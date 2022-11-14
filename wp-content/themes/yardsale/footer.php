42da63a1afc3806d693b8334df62ebd8e77f4f14
conHector <drarkmaster@gmail.com>
2022-11-14 18:36:18 -0300

    Lista la primera importación del template.

---
+++ wp-content/themes/yardsale/footer.php
@@ -0,0 +1,10 @@
+</div>
+    </main>
+
+    <footer class="text-center my-4">
+        De LATAM con 💚 para el mundo
+    </footer>
+    <?php wp_footer(); ?>
+</body>
+
+</html>
---
+++ wp-content/themes/yardsale/functions.php
@@ -0,0 +1,13 @@
+<?php
+
+function chtk_assets () {
+    wp_register_style("google-font", "https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap", array(),false,'all');
+    wp_register_style("google-font-2", "https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap", array(),false,'all');
+    wp_register_style("bootstrap", "https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css", array(), false, 'all');
+
+    wp_enqueue_style("estilos", get_template_directory_uri()."/assets/css/style.css", array('google-font','google-font-2','bootstrap'),false, 'all',);
+
+    wp_enqueue_script("yardsale-js",get_template_directory_uri()."/assets/js/script.js");
+}
+
+add_action("wp_enqueue_scripts","chtk_assets");
