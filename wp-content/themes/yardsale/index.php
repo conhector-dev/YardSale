e4aef085e9869523c4b82e702e6a388b233b2731
conHector <drarkmaster@gmail.com>
2022-11-15 18:26:13 -0300

    Se crea single.php y se implementa en el index.php

--- wp-content/themes/yardsale/index.php
+++ wp-content/themes/yardsale/index.php
@@ -23,13 +23,11 @@
     <?php if($productos->have_posts()) { ?>
         <?php while ($productos->have_posts()) { $productos->the_post(); ?>
             <div class="productos__card">
-                <figure>
-                    <img src="<?php echo get_template_directory_uri();?>/assets/img/prod_1.png" alt="producto 1">
-                </figure>
+                <?php the_post_thumbnail();?>
                 <div class="producto__caption">
                     <div class="productos__desc">
-                        <a class="producto__link" href="#">
-                            <h4 class="productos__titulo">Retro refrigerato</h4>
+                        <a class="producto__link" href="<?php the_permalink();?>">
+                            <h4 class="productos__titulo"><?php the_title();?></h4>
                         </a>
                     </div>
                 </div>
